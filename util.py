54f4d24da048627f58794cf7be323f170d2d4b26
Bristi Maity <118251785+bristi03@users.noreply.github.com>
2024-03-29 17:20:19 +0530

    updated

--- util.py
+++ util.py
@@ -38,13 +38,13 @@ def load_saved_artifacts():
     global __class_name_to_number
     global __class_number_to_name
 
-    with open("./server/artifacts/class_dictionary.json", "r") as f:
+    with open("./artifacts/class_dictionary.json", "r") as f:
         __class_name_to_number = json.load(f)
         __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
 
     global __model
     if __model is None:
-        with open('./server/artifacts/saved_model.pkl', 'rb') as f:
+        with open('./artifacts/saved_model.pkl', 'rb') as f:
             __model = joblib.load(f)
     print("loading saved artifacts...done")
 
@@ -63,8 +63,8 @@ def get_cv2_image_from_base64_string(b64str):
     return img
 
 def get_cropped_image_if_2_eyes(image_path, image_base64_data):
-    face_cascade = cv2.CascadeClassifier('./server/opencv/haarcascades/haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('./server/opencv/haarcascades/haarcascade_eye.xml')
+    face_cascade = cv2.CascadeClassifier('./opencv/haarcascades/haarcascade_frontalface_default.xml')
+    eye_cascade = cv2.CascadeClassifier('./opencv/haarcascades/haarcascade_eye.xml')
 
     if image_path:
         img = cv2.imread(image_path)
@@ -84,7 +84,7 @@ def get_cropped_image_if_2_eyes(image_path, image_base64_data):
     return cropped_faces
 
 def get_b64_test_image_for_virat():
-    with open('./server/b64.txt') as f:
+    with open('./b64.txt') as f:
         return f.read()
     
 if __name__ == '__main__':
@@ -92,9 +92,9 @@ if __name__ == '__main__':
 
     print(classify_image(get_b64_test_image_for_virat(), None))
     
-    print(classify_image(None,"./server/test/vk18.jpg"))
-    print(classify_image(None,"./server/test/msd.jpg"))
-    print(classify_image(None,"./server/test/ronaldo.jpg"))
-    print(classify_image(None,"./server/test/rs.jpg"))
-    print(classify_image(None,"./server/test/ney3.jpg"))
-    print(classify_image(None,"./server/test/mbp.jpg"))
+    print(classify_image(None,"./test/vk18.jpg"))
+    print(classify_image(None,"./test/msd.jpg"))
+    print(classify_image(None,"./test/ronaldo.jpg"))
+    print(classify_image(None,"./test/rs.jpg"))
+    print(classify_image(None,"./test/ney3.jpg"))
+    print(classify_image(None,"./test/mbp.jpg"))
